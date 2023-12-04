0acc7d69aa923a6e2c1dd878942e18b762a66f3c
hyeonjinan <hyeonjinan096@naver.com>
2023-12-04 14:58:33 +0900

    style: DetailMain페이지 css수정

--- src/components/DetailMain.vue
+++ src/components/DetailMain.vue
@@ -25,23 +25,27 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="movieStore.isLoading" class="bg-gray-900 text-white">
-    <LoadingPage />
-  </div>
-  <div class="grid place-items-center bg-gray-900 text-white">
-    <div class="flex p-4 w-4/5 m-10">
-      <img
-        :src="movie.Poster"
-        alt=""
-        class="object-cover rounded-lg mr-4 mb-4 w-4/12" />
-      <div class="w-6/12">
-        <h1 class="text-3xl font-bold mb-2">{{ movie.Title }}</h1>
-        <div class="text-gray-300 mb-2">{{ movie.Director }}</div>
-        <div class="text-gray-400 mb-4">{{ movie.Plot }}</div>
-        <div class="text-gray-300 mb-2">{{ movie.Genre }}</div>
-        <div class="text-gray-300 mb-2">{{ movie.Runtime }}</div>
-        <div class="text-gray-300 mb-2">{{ movie.Released }}</div>
-        <div class="text-purple-500 font-bold">{{ movie.imdbRating }}</div>
+  <div class="h-screen bg-gray-900">
+    <div
+      v-if="movieStore.isLoading"
+      class="bg-gray-900 text-white">
+      <LoadingPage />
+    </div>
+    <div class="grid place-items-center bg-gray-900 text-white">
+      <div class="flex p-4 w-4/5 m-10">
+        <img
+          :src="movie.Poster"
+          alt=""
+          class="object-cover rounded-lg mr-4 mb-4 w-4/12" />
+        <div class="w-6/12">
+          <h1 class="text-3xl font-bold mb-2">{{ movie.Title }}</h1>
+          <div class="text-gray-300 mb-2">{{ movie.Director }}</div>
+          <div class="text-gray-400 mb-4">{{ movie.Plot }}</div>
+          <div class="text-gray-300 mb-2">{{ movie.Genre }}</div>
+          <div class="text-gray-300 mb-2">{{ movie.Runtime }}</div>
+          <div class="text-gray-300 mb-2">{{ movie.Released }}</div>
+          <div class="text-purple-500 font-bold">{{ movie.imdbRating }}</div>
+        </div>
       </div>
     </div>
   </div>
