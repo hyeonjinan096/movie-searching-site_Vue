bbd03577ef559cbb30c583eedb86a0c1dbe54d6c
hyeonjinan <hyeonjinan096@naver.com>
2023-12-05 13:44:54 +0900

    chore: 검색창 placeholder 수정

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue';
 import { useMovieStore } from '../store/movies';
 const movieStore = useMovieStore();
 const title = ref('');
-title.value = 'frozen';
 async function searchMovies() {
   await movieStore.readMovies(title.value);
 }
@@ -18,18 +17,15 @@ function resetMovies() {
     <input
       v-model="title"
       @keydown.enter="searchMovies"
-      class="px-4 py-2 bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-purple-500"
-    />
+      class="px-4 py-2 bg-gray-800 text-white border border-gray-700 focus:outline-none focus:border-purple-500" placeholder="영화를 검색하세요." />
     <button
       @click="searchMovies"
-      class="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition duration-300"
-    >
+      class="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition duration-300">
       Search
     </button>
     <button
       @click="resetMovies"
-      class="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition duration-300"
-    >
+      class="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-800 transition duration-300">
       Reset
     </button>
   </div>
